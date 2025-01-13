b2372f4d274509eee0cd83671cd1475b0428f35d
divantine <divantinechuwa@gmail.com>
2025-01-13 15:27:33 +0300

    Updates

--- opensrp-chw-tbleprosy/src/main/java/org/smartregister/chw/tbleprosy/util/Constants.java
+++ opensrp-chw-tbleprosy/src/main/java/org/smartregister/chw/tbleprosy/util/Constants.java
@@ -21,7 +21,7 @@ public interface Constants {
         String TB_LEPROSY_FOLLOW_UP_VISIT = "TbLeprosy Follow-up Visit";
         String VOID_EVENT = "Void Event";
         String CLOSE_TB_LEPROSY_SERVICE = "Close TbLeprosy Service";
-
+//        String TB_RECORD_TB_CONTACT_VISIT = ""
     }
 
     interface FORMS {
