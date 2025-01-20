2d8fcdd50d22f5036935a746642157c069ed51e1
divantine <divantinechuwa@gmail.com>
2025-01-20 16:25:22 +0300

    Implementation of ContactRegister Activity

--- sample/src/main/java/org/smartregister/chw/tbleprosy_sample/activity/TbLeprosyContactRegister.java
+++ sample/src/main/java/org/smartregister/chw/tbleprosy_sample/activity/TbLeprosyContactRegister.java
@@ -2,59 +2,24 @@ package org.smartregister.chw.tbleprosy_sample.activity;
 
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.smartregister.chw.tbleprosy.activity.BaseTbLeprosyProfileActivity;
 import org.smartregister.chw.tbleprosy_sample.R;
 
-import timber.log.Timber;
-
-public class TbLebrosyContactRegister extends BaseTbLeprosyProfileActivity {
+public class TbLeprosyContactRegister  extends AppCompatActivity {
     @Override
-    protected void setupButtons() {
-        super.setupButtons();
-    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_contact);
 
-    @Override
-    public void openFormProfile() {}
 
-    @Override
-    public void openTbLeprosyContactRegister() {}
+        findViewById(R.id.newClient);
+        findViewById(R.id.existingClient);
 
-    @Override
-    public void openRecordTbContactVisit() {}
-
-    @Override
-    public void openClientObservationResults() {
-    }
-
-    @Override
-    public void observationResults() {
-    }
-
-    @Override
-    public void openTbContactFollowUpVisit() {
 
     }
 
-    @Override
-    public void openRecordClientVisit() {
 
-    }
-
-    @Override
-    public void startServiceForm() {
-
-    }
-
-    @Override
-    public void continueService() {
-
-    }
-
-    @Override
-    public void continueDischarge() {
-
-    }
 }
