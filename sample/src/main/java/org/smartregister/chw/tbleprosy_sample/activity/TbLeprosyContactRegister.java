fef0abb97c63436fb141f1dfb4eaefd3328c102d
divantine <divantinechuwa@gmail.com>
2025-01-20 16:18:09 +0300

    To add ContactRegister Activity

---
+++ sample/src/main/java/org/smartregister/chw/tbleprosy_sample/activity/TbLeprosyContactRegister.java
@@ -0,0 +1,60 @@
+package org.smartregister.chw.tbleprosy_sample.activity;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.smartregister.chw.tbleprosy.activity.BaseTbLeprosyProfileActivity;
+import org.smartregister.chw.tbleprosy_sample.R;
+
+import timber.log.Timber;
+
+public class TbLebrosyContactRegister extends BaseTbLeprosyProfileActivity {
+    @Override
+    protected void setupButtons() {
+        super.setupButtons();
+    }
+
+    @Override
+    public void openFormProfile() {}
+
+    @Override
+    public void openTbLeprosyContactRegister() {}
+
+    @Override
+    public void openRecordTbContactVisit() {}
+
+    @Override
+    public void openClientObservationResults() {
+    }
+
+    @Override
+    public void observationResults() {
+    }
+
+    @Override
+    public void openTbContactFollowUpVisit() {
+
+    }
+
+    @Override
+    public void openRecordClientVisit() {
+
+    }
+
+    @Override
+    public void startServiceForm() {
+
+    }
+
+    @Override
+    public void continueService() {
+
+    }
+
+    @Override
+    public void continueDischarge() {
+
+    }
+}
