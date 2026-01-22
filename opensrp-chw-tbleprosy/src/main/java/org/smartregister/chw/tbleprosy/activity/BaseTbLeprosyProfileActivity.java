573cb5a6f119c785d5e002ddd6b6138fa98136e9
cozej4 <ilakozejumanne@gmail.com>
2026-01-22 18:32:47 +0300

    🐛 Fix age display on TB/Leprosy profile

--- opensrp-chw-tbleprosy/src/main/java/org/smartregister/chw/tbleprosy/activity/BaseTbLeprosyProfileActivity.java
+++ opensrp-chw-tbleprosy/src/main/java/org/smartregister/chw/tbleprosy/activity/BaseTbLeprosyProfileActivity.java
@@ -305,8 +305,7 @@ public abstract class BaseTbLeprosyProfileActivity extends BaseProfileActivity i
     @SuppressLint("DefaultLocale")
     @Override
     public void setProfileViewWithData() {
-        int age = new Period(new DateTime(memberObject.getAge()), new DateTime()).getYears();
-        textViewName.setText(String.format("%s %s %s, %d", memberObject.getFirstName(), memberObject.getMiddleName(), memberObject.getLastName(), age));
+        textViewName.setText(String.format("%s %s %s, %d", memberObject.getFirstName(), memberObject.getMiddleName(), memberObject.getLastName(), memberObject.getAge()));
         textViewGender.setText(TbLeprosyUtil.getGenderTranslated(this, memberObject.getGender()));
         textViewLocation.setText(memberObject.getAddress());
         textViewUniqueID.setText(memberObject.getUniqueId());
